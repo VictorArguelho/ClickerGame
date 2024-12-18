0e50690e0254bce75e64d48ee4e70f42d228e455
PomboTurbinado <varguelho9@gmail.com>
2024-12-18 01:47:56 -0300

    hotfix/construtor-gameobject

--- GameCore/GameObject.cs
+++ GameCore/GameObject.cs
@@ -13,7 +13,9 @@ namespace GameCore
         public string Name { get; set; }
         public string Tag { get; set; }
 
+            #pragma warning disable CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
         public GameObject(string name, string tag)
+            #pragma warning restore CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
         {
             Name = name;
             Tag = tag;
