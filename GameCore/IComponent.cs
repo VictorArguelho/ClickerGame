994425a3487189a10a3977fdbfc65c09c6d51e23
PomboTurbinado <varguelho9@gmail.com>
2024-12-18 01:08:44 -0300

    Adução das interfaces IComponent e IRenderable

---
+++ GameCore/IComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameCore
+{
+    public interface IComponent
+    {
+        GameObject Parent { get; set; }
+
+        void Start();
+        void Update();
+    }
+}
